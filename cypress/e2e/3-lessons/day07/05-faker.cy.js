7b1b30a226c834c572806ece125671112af8a8f6
Furkan Kılıc <fkrx928@gmail.com>
2024-01-25 23:25:43 +0300

    My

--- cypress/e2e/3-lessons/day07/05-faker.cy.js
+++ cypress/e2e/3-lessons/day07/05-faker.cy.js
@@ -1,9 +1,26 @@
-const { faker } = require("@faker-js/faker");//Parantez içerisine (@faker-js/faker) bu data yı koyduktan sonra süslü paranteze faker yaz
+const { faker } = require("@faker-js/faker");
 let fakeName = faker.person.firstName()
-let lasetName = faker.person.lastName()
-let fakeMail = faker.internet.fakeMail()
-let password = faker.internet.password()
-
-
-
-constant
+let fakeLastName = faker.person.lastName()
+let fakeEmail = faker.internet.email()
+let fakePassword = faker.internet.password()
+describe('FakerJS Kullanimi', () => {
+    it('FakerJS ile Ornek Test', () => {
+        cy.visit('https://www.kitapyurdu.com/index.php?route=account/register');
+        cy.get('#register-form').within(() => {
+            cy.get('#register-name').type(fakeName)
+            cy.get('#register-lastname').type(fakeLastName)
+            cy.get('#register-email').type(fakeEmail)
+            cy.get('#register-password').type(fakePassword)
+            cy.get('#register-password-confirm').type(fakePassword)
+        })
+    });
+    it.only('Eposta Validation Mesajinin dogrulanmasi', () => {
+        cy.visit('https://www.automationexercise.com/login');
+        cy.get('[data-qa="login-email"]').type(fakeName)
+        cy.get('[data-qa="login-password"]').type(fakePassword)
+        cy.get('[data-qa="login-button"]').click()
+        cy.get('[data-qa="login-email"]')
+            .invoke('prop', 'validationMessage')
+            .should('eq', `Lütfen eposta adresine bir "@" işareti ekleyin. "${fakeName}" adresinde "@" eksik.`)
+    });
+});
