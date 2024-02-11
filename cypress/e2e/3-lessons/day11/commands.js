c396e103bb934f6bc6526b8fd1b50dcd36a6b253
Furkan Kılıc <fkrx928@gmail.com>
2024-02-11 15:00:08 +0300

    yeni

---
+++ cypress/e2e/3-lessons/day11/commands.js
@@ -0,0 +1,48 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('magentoLogin', (email, password) => {
+    cy.contains('Sign In').click()
+    cy.get('#email').type(email)
+    cy.get('#pass').type(password)
+    cy.get('#send2').click()
+})
+
+Cypress.Commands.add('magentoArama', (kelime) => {
+    cy.get('#search').type(kelime)
+    cy.get('[title="Search"]').click();
+    cy.get('.base').should('be.visible').and('contain', kelime)
+})
+
+Cypress.Commands.add('orangehrmSessionLogin', (username, password) => {
+    cy.session([username, password], () => {
+        cy.visit('https://opensource-demo.orangehrmlive.com/');
+        cy.get('[name="username"]').type(username)
+        cy.get('[name="password"]').type(password)
+        cy.get('[type="submit"]').click()
+    })
+})
+
+
---
+++ cypress/e2e/3-lessons/day11/cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  experimentalStudio: true, // Cypress Studio kullanımı için bu ayarı açtık
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: "https://practicesoftwaretesting.com/#/",
+    env: {
+      signin: "auth/login",
+      contact: "contact",
+      amazon: "https://www.amazon.com",
+    }
+  },
+});
